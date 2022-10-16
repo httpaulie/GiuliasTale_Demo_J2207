dd306d36febc30da04b12771458cb2986417b3f1
felipedoidao <felipexd02@gmail.com>
2022-10-15 23:32:49 -0300

    Sprites e Animação

--- J2207/Assets/Scripts/Player.cs
+++ J2207/Assets/Scripts/Player.cs
@@ -4,20 +4,40 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Animator anim;
     public Rigidbody2D playerRb;
     public float speed = 5f;
     Vector2 movimento;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        move();
+        Animation();
+    }
+
+    void move()
+    {
         movimento.x = Input.GetAxisRaw("Horizontal");
         movimento.y = Input.GetAxisRaw("Vertical");
         playerRb.MovePosition(playerRb.position + movimento * speed * Time.fixedDeltaTime);
+    }
+
+    void Animation()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+
+        Vector2 offset = new Vector2(mousePos.x - screenPoint.x, mousePos.y - screenPoint.y);
+
+        anim.SetBool("mouseUp", (offset.y>50));
+        anim.SetBool("mouseDown", (offset.y<-50));
+        anim.SetBool("mouseRight", (offset.x>50));
+        anim.SetBool("mouseLeft", (offset.x<-50));
     }
 }
