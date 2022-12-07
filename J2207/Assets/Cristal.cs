98613699756c9aeb567fa4270f75515bd9fbe2f9
felipedoidao <felipexd02@gmail.com>
2022-12-07 17:30:21 -0300

    pronto!

--- J2207/Assets/Cristal.cs
+++ J2207/Assets/Cristal.cs
@@ -7,8 +7,11 @@ public class Cristal : MonoBehaviour
 {
     public string sceneName;
 
-    void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerStay2D(Collider2D other)
     {
+        if(other.CompareTag("Player"))
+        {
         SceneManager.LoadScene(sceneName);
+        }
     }
 }
